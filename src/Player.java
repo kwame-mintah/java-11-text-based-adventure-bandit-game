383a30713d395064af32220b6d2a701bef45fb13
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-07-21 20:46:15 +0100

    feat(project): initial commit to version control
    move master branch of previous github account to new github account

---
+++ src/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+
+    boolean playerOwnsHorse = false;
+    boolean mask = false;
+    int health = 100;
+    int money = 50;
+    int notoriety = 0;
+    int strength = 1;
+
+}
